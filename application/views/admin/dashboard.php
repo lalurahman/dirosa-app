6ba714cdd9ed464a7f003f0d0ecc0c76d7234693
Lalu Abdurrahman <34390193+lalurahman@users.noreply.github.com>
2020-02-07 13:59:15 +0800

    baru

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -54,15 +54,11 @@
                 <div class="col s12 m6 l3">
                     <div class="card gradient-45deg-green-teal gradient-shadow min-height-100 white-text">
                         <div class="padding-4">
-                            <div class="col s7 m7">
-                                <i class="material-icons background-round mt-5">attach_money</i>
-                                <p>Profit</p>
+                            <div class="col s12 m12 center">
+                                <h3 class="mt-5"><?= date('H:i:s') ?></h3>
+                                <p class="mt-0"><?= date('l, d-M-Y') ?></p>
                             </div>
-                            <div class="col s5 m5 right-align">
-                                <h5 class="mb-0">$890</h5>
-                                <p class="no-margin">Today</p>
-                                <p>$25,000</p>
-                            </div>
+
                         </div>
                     </div>
                 </div>
